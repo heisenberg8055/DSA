b3195fa1f73f03b7815a3e4b193e5881830e4f41
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2023-06-06 10:51:53 +0530

    Time: 4 ms (74.54%), Space: 9.1 MB (28.46%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
         int n = arr.size();
-        unordered_set<int>s;
-        int mini = *min_element(arr.begin(), arr.end());
-        int maxi = *max_element(arr.begin(), arr.end());
+        int mini = *min_element(arr.begin(),arr.end());
+        int maxi = *max_element(arr.begin(),arr.end());
         if(maxi - mini == 0)
         {
             return true;
@@ -13,20 +12,35 @@ public:
         {
             return false;
         }
-        int diff = (maxi - mini) / (n - 1);
-        for(int i = 0;i<n;i++)
+        int diff = (maxi - mini) / (n-1);
+        if(diff == 0)
         {
-            if((arr[i] - mini) % diff != 0)
+            return true;
+        }
+        int i = 0;
+        while(i<n)
+        {
+            if(arr[i] == mini +(i*diff))
+            {
+                i++;
+                continue;
+            }
+            else if((arr[i] - mini) % diff != 0)
             {
                 return false;
             }
-            else{
-                s.insert(arr[i]);
+            else
+            {
+                int j = ((arr[i] - mini) /diff);
+                if(arr[i] == arr[j])
+                {
+                    return false;
+                }
+                else
+                {
+                    swap(arr[i], arr[j]);
+                }   
             }
-        }
-        if(s.size() != n)
-        {
-            return false;
         }
         return true;
     }
