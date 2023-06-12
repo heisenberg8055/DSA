7605a55db09590fc0610d0cd26d42e029192075f
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2023-06-12 12:57:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (61.25%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        int dp[n+2];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2;i<=n;i++)
+        int ans = 0;
+        int n0 = 0;
+        int n1 = 1;
+        if(n == 0 || n == 1 )
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            return n;
         }
-        return dp[n];
+        for(int i = 2; i<=n; i++)
+        {
+            ans = n0 + n1;
+            n0 = n1;
+            n1 = ans;
+        }
+        return ans;
     }
 };
