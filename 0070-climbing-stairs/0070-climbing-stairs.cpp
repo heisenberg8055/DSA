ac519ebd4cdb89ad165ced4e3f0a085cf9a69d67
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2023-06-13 11:36:48 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,26 +1,14 @@
 class Solution {
-private:
-    int trav(int n, int dp[])
-    {
-        if(n<0)
-        {
-            return 0;
-        }
-        if(n == 0 || n == 1)
-        {
-            return dp[n] = 1;
-        }
-        if(dp[n])
-        {
-            return dp[n];
-        }
-        return dp[n] = trav(n-1, dp) + trav(n-2, dp);
-    }
 public:
     int climbStairs(int n) {
         int dp[n+1];
-        memset(dp,0,sizeof(dp));
-        trav(n,dp);
+        memset(dp, 0 ,sizeof(dp));
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 };
