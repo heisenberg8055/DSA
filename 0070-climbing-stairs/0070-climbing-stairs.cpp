61ea524d4de31be1837aabee0569986297971e0d
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2023-06-14 10:42:00 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 1)
-        {
+        if(n == 1){
             return 1;
         }
-        int ans = 0;    
+        int ans = 0;
         int f = 1;
         int s = 1;
-        for(int i = 2;i<=n;i++)
-        {
-            ans = f+s;
+        for(int i = 2; i<=n ;i++){
+            ans = f + s;
             f = s;
             s = ans;
         }
