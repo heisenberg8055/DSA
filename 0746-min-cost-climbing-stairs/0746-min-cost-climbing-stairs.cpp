e48f22b78daebf42517f8dc5987a5f8f913a2099
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2023-06-14 11:27:44 +0530

    Time: 3 ms (94.25%), Space: 13.6 MB (93.82%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        int dp[n+2];
-        memset(dp, 0, sizeof(dp));
-        dp[n] = 0, dp[n+1] = 0;
-        for(int i = n-1; i >= 0; i--)
+        for(int i = 2; i<n;i++)
         {
-            dp[i] = cost[i] + min(dp[i+1] , dp[i+2]);
+            cost[i] += min(cost[i-1], cost[i-2]);
         }
-        return dp[0] > dp[1] ? dp[1] : dp[0];
+        return min(cost[n-1], cost[n-2]);
     }
 };
