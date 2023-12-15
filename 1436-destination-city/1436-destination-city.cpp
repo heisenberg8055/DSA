c6aa455b8a2815661ea6730949020d11967b096a
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2023-12-15 10:53:09 +0530

    Time: 8 ms (75.96%), Space: 10.8 MB (85.89%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -3,17 +3,14 @@ public:
     string destCity(vector<vector<string>>& paths) {
         int n = paths.size();
         unordered_set<string>s;
-        unordered_set<string>e;
         for(int i = 0; i < n; i++)
         {
-            s.insert(paths[i][0]);
-            e.erase(paths[i][0]);
-            e.insert(paths[i][1]);
+            s.insert(paths[i][1]);
         }
-        for(auto it: s)
+        for(int i = 0; i < n; i++)
         {
-            e.erase(it);
+            s.erase(paths[i][0]);
         }
-        return *e.begin();
+        return *s.begin();
     }
 };
