1b993a4985c61411ddd3f6bb178844165d862142
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2024-01-11 13:11:04 +0530

    Time: 3 ms (85.41%), Space: 10.1 MB (79.85%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -9,27 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution{
-private:
-    void trav(TreeNode* root, int &ans, int mini, int maxi)
-    {
-        if(root == NULL)
-        {
-            return;
-        }
-        mini = min(mini, root->val);
-        maxi = max(maxi, root->val);
-        if(mini!= 5002 && maxi)
-        {
-            ans = max(ans, abs(maxi - mini));
-        }
-        trav(root->left,ans,mini,maxi);
-        trav(root->right,ans,mini,maxi);
-    }
+class Solution {
 public:
-    int maxAncestorDiff(TreeNode* root) {
-        int ans = 0;
-        trav(root,ans,5002,0);
-        return ans;
+    int maxAncestorDiff(TreeNode* root, int mx = 0, int mn = 5002) {
+        return root == NULL ? mx - mn : max(maxAncestorDiff(root->left,max(mx,root->val),min(mn,root->val)),maxAncestorDiff(root->right,max(mx,root->val),min(root->val,mn)));
     }
 };
