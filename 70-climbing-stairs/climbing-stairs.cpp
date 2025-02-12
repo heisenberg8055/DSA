0deb95989f25970b5323e64d3f98be4adbb1b478
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-02-12 12:55:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (57.70%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
-private:
-    int trav(int n, vector<int> &dp) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        return dp[n] = trav(n - 1, dp) + trav(n - 2, dp);
-    }
 public:
     int climbStairs(int n) {
-        vector<int>dp(n + 1, -1);
-        return trav(n, dp);
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+        int prev = 1, curr = 1;
+        for(int i = 2; i <= n; i++) {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+        }
+        return curr;
     }
 };
