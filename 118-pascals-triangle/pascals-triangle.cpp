0dc0b703786dbb3789e6338606296f9454d242dc
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-02-12 13:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (64.44%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>ans;
+        for(int i = 1; i <= numRows; i++) {
+            vector<int>temp(i, 1);
+            if (i != 1 && i != 2) {
+                for(int j = 1; j < i - 1; j++)
+                {
+                    temp[j] = ans.back()[j - 1] + ans.back()[j];
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
