4d34336aecf6095d5a1f5ff028a59759bfb7d515
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-02-14 09:57:23 +0530

    Time: 99 ms (2.96%) | Memory: 290.7 MB (5.24%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,24 +1,25 @@
 class Solution {
+private:
+    int isLCS(string s, string t, int i, int j, vector<vector<int>> &dp) {
+        if (i == 0 || j == 0) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (s[i - 1] == t[j - 1]) {
+            return dp[i][j] = 1 + isLCS(s, t, i - 1, j - 1, dp);
+        }
+        return dp[i][j] = isLCS(s, t, i, j - 1, dp);
+    }
 public:
     bool isSubsequence(string s, string t) {
-        int n = s.size(), m = t.size();
-        if(n > m) {
+        int n = s.size();
+        int m = t.size();
+        if (n > m) {
             return false;
         }
-        int i = 0, j = 0;
-        while(i < n && j < m) {
-            while(j < m && t[j] != s[i]) {
-                j++;
-            }
-            if(t[j] == s[i]){
-
-            
-            i++;
-            j++;
-            } else{
-                break;
-            }
-        }
-        return i == n;
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, -1));
+        return isLCS(s, t, n, m, dp) == n;
     }
 };
