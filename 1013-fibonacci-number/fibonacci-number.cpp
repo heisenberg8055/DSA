12d1718fe0666a432ef22dc4cade9cad444307b2
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-02-19 14:04:38 +0530

    Time: 3 ms (67.99%) | Memory: 8.3 MB (9.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,15 @@
 class Solution {
-    vector<int>dp;
-private:
-    int help(int n) {
+public:
+    int fib(int n) {
         if (n < 2) {
             return n;
         }
-        if (dp[n] != -1) {
-            return dp[n];
+        vector<int>dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n] = fib(n - 1) + fib(n - 2);
-    }
-public:
-    int fib(int n) {
-        dp.resize(n + 1, -1);
-        return help(n);
+        return dp[n];
     }
 };
