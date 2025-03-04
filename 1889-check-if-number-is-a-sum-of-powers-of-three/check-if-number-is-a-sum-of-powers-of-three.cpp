8edd610761976b8f6e8a7a9521503d4202ad91a0
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-03-04 08:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (67.84%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,26 +1,12 @@
 class Solution {
-private:
-    bool help(int i, int curr, int n, vector<int>& nums) {
-        if(curr == n) {
-            return true;
-        } else if ( curr > n) {
-            return false;
-        }
-        if (i < 0) {
-            return curr == n;
-        }
-        bool p = help(i - 1, curr + nums[i], n, nums);
-        bool nP = help(i - 1, curr, n, nums);
-        return nP || p;
-    }
 public:
     bool checkPowersOfThree(int n) {
-        vector<int>nums;
-        int reff = 0;
-        while(pow(3, reff) <= n) {
-            nums.push_back(pow(3, reff));
-            reff++;
+        while(n) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
         }
-        return help(nums.size() - 1, 0, n, nums);
+        return true;
     }
 };
