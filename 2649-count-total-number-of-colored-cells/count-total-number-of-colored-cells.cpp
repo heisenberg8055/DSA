c8f063a421353a35670778ad467314385e90c648
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-03-05 09:12:21 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.17%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,7 +1,7 @@
+typedef long long ll;
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans = (2 *  pow(n - 1, 2)) + ((2 * n) - 1);
-        return ans;
+        return 1ll * (n) * (n) + 1ll * (n - 1) * (n - 1);
     }
 };
