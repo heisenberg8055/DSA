971af8c7093cde732d5d9e5684c8a9b8a13c717e
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-03-05 10:27:46 +0530

    Time: 182 ms (12.25%) | Memory: 12.8 MB (94.02%) - LeetSync

--- 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
+++ 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.cpp
@@ -4,18 +4,21 @@ public:
         int ans = 0;
         int m = students.size();
         int n = students[0].size();
-        sort(students.begin(), students.end());
+        vector<int>pos;
+        for(int i = 0; i < m; i++) {
+            pos.push_back(i);
+        }
         do {
             int cnt = 0;
             for(int i = 0; i < m; i++) {
                 for(int j = 0; j < n; j++) {
-                    if (students[i][j] == mentors[i][j]) {
+                    if (students[pos[i]][j] == mentors[i][j]) {
                         cnt++;
                     }
                 }
             }
             ans = max(ans, cnt);
-        }while (next_permutation(students.begin(), students.end()));
+        }while (next_permutation(pos.begin(), pos.end()));
         return ans;
     }
 };
