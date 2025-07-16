d1ad3833d32befdd12d0c58e581d0b49b72153fc
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2025-07-16 18:10:14 +0530

    Time: 11 ms (32.94%) | Memory: 96.2 MB (62.35%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        int ans = 0;
-        int n = nums.size(), c = 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(nums[i] % 2 == 0)
-            {
-                c++;
+        int n = nums.size(), c = nums[0]%2, o = 0, e = 0, b = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] % 2 == 0) {
+                e++;
+            } else {
+                o++;
+            }
+            if(nums[i] % 2 == c) {
+                b++;
+                c ^= 1;
             }
         }
-        ans = max(ans, c);
-        c = 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(nums[i] % 2)
-            {
-                c++;
-            }
-        }
-        ans = max(ans, c);
-        c = 0;
-        int reff = nums[0] % 2;
-        for(int i = 0; i < n; i++)
-        {
-            if(nums[i] % 2 == reff)
-            {
-                c++;
-                reff ^= 1;
-            }
-        }
-        ans = max(ans, c);
-        return ans;
+        return max(b, max(e, o));
     }
 };
