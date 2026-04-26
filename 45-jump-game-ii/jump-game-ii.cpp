7917637caedf14d2b419f18c3fb0248502f7c963
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2026-04-26 22:02:33 +0530

    Time: 68 ms (16.60%) | Memory: 21.5 MB (19.47%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int>dp(size(nums), 10001);
-        return solve(nums, dp, 0);
-    }
-    int solve(vector<int>& nums, vector<int>& dp, int pos) {
-        if(pos >= size(nums) - 1) return 0;
-        if(dp[pos] != 10001) {
-            return dp[pos];
+        int n = size(nums);
+        vector<int>dp(n, 10001);
+        dp[n - 1] = 0;
+        for(int i = n - 2; i >= 0; i--) {
+            for(int jumpLen = 1; jumpLen <= nums[i]; jumpLen++) {
+                dp[i] = min(dp[i], 1 + dp[min(n - 1, i + jumpLen)]);
+            }
         }
-        for(int j = 1; j <= nums[pos]; j++) {
-            dp[pos] = min(dp[pos], 1 + solve(nums, dp, pos + j));
-        }
-        return dp[pos];
+        return dp[0];
     }
 };
