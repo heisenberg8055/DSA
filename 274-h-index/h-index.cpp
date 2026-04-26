42347e8b51ed08f99ccd444af2e698d17ae57669
Yeswanth Sai Inturi <83918511+heisenberg8055@users.noreply.github.com>
2026-04-26 16:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (21.25%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        sort(citations.begin(), citations.end());
-        for(int i = n; i >= 1; i--) {
-            if(citations[n - i] >= i) {
-                return i;
+        int papers = citations.size();
+        vector<int>citationBuckets(papers + 1, 0);
+        for(auto it: citations) {
+            citationBuckets[min(papers, it)]++;
+        }
+        int cumPapers = 0;
+        for(int hIndex = papers; hIndex >= 0; hIndex--) {
+            cumPapers += citationBuckets[hIndex];
+            if(cumPapers >= hIndex) {
+                return hIndex;
             }
         }
         return 0;
